f97db63004228460277c98a4740df214d4d6792e
jinal90 <jinal010990@gmail.com>
2016-12-10 01:33:29 +0530

    created fragments and main activity

---
+++ app/src/main/java/com/haptic/chatlist/ui/ChatFragment.java
@@ -0,0 +1,33 @@
+package com.haptic.chatlist.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.haptic.chatlist.R;
+
+public class ChatFragment extends Fragment {
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+    public static ChatFragment newInstance() {
+        ChatFragment fragment = new ChatFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+}
--- app/src/main/java/com/haptic/chatlist/ui/MainActivity.java
+++ app/src/main/java/com/haptic/chatlist/ui/MainActivity.java
@@ -1,13 +1,21 @@
-package com.haptic.chatlist;
+package com.haptic.chatlist.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.haptic.chatlist.R;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setupUI(){
+
     }
 }
